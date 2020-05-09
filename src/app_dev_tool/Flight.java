2f822840cdb746e71ad9ec8fbc2327d4c5dc550a
לירון <@DESKTOP-PB2GMQF>
2020-05-09 20:16:28 +0300

    test

--- src/app_dev_tool/Flight.java
+++ src/app_dev_tool/Flight.java
@@ -12,7 +12,7 @@ public class Flight {
 	private int year;
 	private String destination;
 	private double price;
-	private String l = "liron";
+	
 
 	public Flight(int flightId, int hour, int min, int day, int month, int year, String destination, double price) {
 		this.flightId = flightId;
